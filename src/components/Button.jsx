4a67a8ef541b536f805f7c680fa6cd83480d6d88
Jones Guidini <jonesguidini@hotmail.com>
2020-02-14 11:15:30 -0300

    Feat: adição de comentários

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,15 +2,19 @@ import React from 'react'
 import './Button.css'
 
 export default props => {
-    let classes = 'button '
+    let classes = 'button'
     
-    classes += props.operation ? 'operation' : '' 
-    classes += props.double ? 'double' : ''
-    classes += props.triple ? 'triple' : ''  
+    classes += props.operation ? ' operation' : '' 
+    classes += props.double ? ' double' : ''
+    classes += props.triple ? ' triple' : ''  
 
     return(
         <button 
-        onClick={e => props.click && props.click(props.label)}
+        onClick={
+            e => 
+            props.click // executa 'operacao'
+            && //senão
+            props.click(props.label)} //adiciona o valor 'label' no display
         className={classes}>
         {props.label}
         </button>
--- src/main/Calculator.jsx
+++ src/main/Calculator.jsx
@@ -8,64 +8,68 @@ const initialState = {
   clearDisplay: false,
   operation: null,
   values: [0, 0],
-  current: 0
+  currentPosition: 0
 };
 
 class Calculator extends Component {
-  state = { ...initialState };
+  state = { ...initialState }; // faz clone do stado conforme o schema 'initialState' usando o spread operator
 
   constructor(props) {
     super(props);
+    // abaixo binda os métodos direto no contexto 'this' da classe
     this.clearMemmory = this.clearMemmory.bind(this);
     this.setOperation = this.setOperation.bind(this);
     this.addDigit = this.addDigit.bind(this);
   }
 
   clearMemmory() {
-    this.setState({ ...initialState });
+    this.setState({ ...initialState }); // para limpar o state aplica o schema anterior limpo
   }
 
   setOperation(operation) {
-    if (this.state.current === 0) {
-      this.setState({ operation, current: 1, clearDisplay: true });
+    if (this.state.currentPosition === 0) { // caso não tenha sido informado nenhuma operação anteriormente (valor igual a )
+      this.setState({ operation, currentPosition: 1, clearDisplay: true }); // atualiza o state passando a nova operação, atualizando a currentPosition para 1 (já q agora tem uma operacação) e seta variável para limpar display para true (essa variável será verificada antes de entrar o próximo digito)
     } else {
-      const equals = operation === "=";
-      const currentOperantion = this.state.operation;
+      const equals = operation === "="; // define verdadeiro caso a operação corrente seja o '=' senão atribui o valor da variável para falso
+      const currentOperantion = this.state.operation; // cria variável de ref da operação atual
 
-      const values = [...this.state.values];
+      const values = [...this.state.values]; //copia o array de valores
 
       try {
-        // mudar o EVAL por um swtich, testando qual é a operação e aplicando o calculo conforme a operação informada
-        values[0] = eval(`${values[0]} ${currentOperantion} ${values[1]}`);
-      } catch (error) {
-        values[0] = this.state.values[0];
+
+        // A função eval() computa um código JavaScript representado como uma string. - Ref: https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/eval
+        // Sugestão: mudar o EVAL por um swtich, testando qual é a operação e aplicando o calculo conforme a operação informada
+        values[0] = eval(`${values[0]} ${currentOperantion} ${values[1]}`); //executa a operação conforme valores 0 e 1 e a operação informada E SALVA o resultado na posição 0 do array de values novamente
+      } catch (error) { // se houver algum erro ao tentar executar a operação acima
+        values[0] = this.state.values[0]; //volta o valor do array 0 para o valor default, ou seja, '0'
       }
 
-      values[1] = 0;
+      values[1] = 0; // muda o valor na posição 1 para 0 novamente após executar a operação falhando ou não (para que a próxima sequência comece da posição 0 novamente)
 
+      // abaixo atualiza o state com base na operação realizada
       this.setState({
-        displayValue: values[0],
-        operation: equals ? null : operation,
-        current: equals ? 0 : 1,
-        clearDisplay: !equals,
-        values
+        displayValue: values[0], // mostra o valor atualizado conforme operação
+        operation: equals ? null : operation, // se variável equals for true (se a operacao for '=') então atualiza a operação com null (reseta a operação), senão atualiza a operação para a informada
+        currentPosition: equals ? 0 : 1, // atualiza a posição do ponteiro do array de valroes conforme o tipo da operação atual ser '=' ou não
+        clearDisplay: !equals, //seta para limpar o display ou não conforme for '=' ou não
+        values //atualiza o array de valores
       });
     }
   }
 
   addDigit(n) {
-    if (n === "." && this.state.displayValue.includes(".")) {
+    // verifica se o digito informado é '.' e se já contém '.' no display, se já houver ele retorna null parando a sequência
+    if (n === "." && this.state.displayValue.includes(".")) { // o includes aqui equiva-le ao Contains
       return;
     }
 
-    const clearDisplay =
-      this.state.displayValue === "0" || this.state.clearDisplay;
+    const clearDisplay = this.state.displayValue === "0" || this.state.clearDisplay; 
     const currentValue = clearDisplay ? "" : this.state.displayValue;
     const displayValue = currentValue + n;
     this.setState({ displayValue, clearDisplay: false });
 
     if (n !== ".") {
-      const i = this.state.current;
+      const i = this.state.currentPosition;
       const newValue = parseFloat(displayValue);
       const values = [...this.state.values];
       values[i] = newValue;
